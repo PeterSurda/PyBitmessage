8282228188dd8807e8debe606c0e4bf4f7933f52
Peter Surda <peter@bitmessage.at>
2024-03-26 08:08:03 +0800

    Android mock AAB

--- .buildbot/android/build.sh
+++ .buildbot/android/build.sh
@@ -2,8 +2,9 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 pushd packages/android
-buildozer android debug || exit $?
+buildozer android release || exit $?
 popd
 
 mkdir -p ../out
-cp packages/android/bin/*.apk ../out
+release_artifact=$(grep release_artifact packages/android/buildozer.spec |cut -d= -f2|tr -Cd 'a-z')
+cp packages/android/bin/*.${release_artifact} ../out
--- .buildbot/android/test.sh
+++ .buildbot/android/test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+release_artifact=$(grep release_artifact packages/android/buildozer.spec |cut -d= -f2|tr -Cd 'a-z')
+
+if [ $release_artifact = "aab" ]; then
+	exit
+fi
+
 unzip -p packages/android/bin/*.apk assets/private.tar \
     | tar --list -z > package.list
 cat package.list
